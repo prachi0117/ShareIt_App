d6d35bb5d068a7036cff36232b57f1230506ca08
prachi gupta <prachiannie17@gmail.com>
2024-07-30 18:36:38 +0530

    Initial commit

---
+++ api/index.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv")
+const mongoose = require("mongoose");
+const { default: helmet } = require("helmet");
+const morgan = require("morgan");
+dotenv.config();
+const useRoute = require("./routes/users")
+const authRoute = require("./routes/auth")
+const postRoute = require("./routes/posts")
+const multer = require("multer")
+const path = require("path");
+
+
+mongoose.connect('mongodb://localhost:27017/socialapp', {
+})
+.then(() => {
+     console.log('Connected to MongoDB');
+})
+.catch(err => {
+    console.error('Failed to connect to MongoDB', err);
+});
+
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+
+//middleware
+app.use(express.json());
+app.use(helmet())
+app.use(morgan("comman"))
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, path.join(__dirname, "public/images"));
+    },
+    filename: (req, file, cb) => {
+      cb(null, req.body.name || file.originalname);
+    },
+  });
+ 
+const upload = multer({storage});
+app.post("/api/upload", upload.single("file"),(req,res) =>{
+    try{
+        return res.status(200).json("File uploaded successfully.")
+
+    }catch(err){
+        console.error('Upload error:', err);
+        res.status(500).json('Error uploading file.');
+
+    }
+
+})
+
+app.use("/api/users",useRoute);
+app.use("/api/auth",authRoute);
+app.use("/api/posts",postRoute);
+
+
+app.get('/' , (req,res) => {
+    res.send("helo");
+})
+
+app.listen(8800, () =>{
+    console.log("Backend server is running");
+})
